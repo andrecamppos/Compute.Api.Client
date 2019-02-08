5fb98cee9a4933820d8694ba60c5e0d5c1167847
Adnan Akbar <adnan.akbar@itaas.dimensiondata.com>
2019-02-08 12:17:36 +1100

    trigger a new build.

--- ComputeClient/Compute.Client/StringExtensions.cs
+++ ComputeClient/Compute.Client/StringExtensions.cs
@@ -8,7 +8,6 @@
 // --------------------------------------------------------------------------------------------------------------------
 
 
-
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
